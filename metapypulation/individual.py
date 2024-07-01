cace36e58466b3506a87924709d89b03b0b3d99c
Matteo Tomasini <matteo.tomasini@lir.gu.se>
2024-07-01 15:58:21 +0200

    base subpopulation and individual created

---
+++ metapypulation/individual.py
@@ -0,0 +1,8 @@
+"""
+A module containing the individual class.
+"""
+
+class Individual():
+    def __init__(self, id: int, original_deme_id: int):
+        self.id = id
+        self.original_deme_id = original_deme_id
--- metapypulation/subpopulation.py
+++ metapypulation/subpopulation.py
@@ -2,3 +2,36 @@
 A module containing the subpopulation class.
 """
 
+from collections.abc import MutableSet
+import numpy as np
+from typing import Tuple
+
+from .individual import Individual
+
+class Subpopulation():
+    def __init__(self, id: int, coordinates: Tuple[int, int], migration_mapping: np.ndarray = None):
+        self.id = id
+        self.coordinates = coordinates
+        self.migration_mapping = migration_mapping
+        self.individuals = IndividualsInPopulation(self)
+        
+
+class IndividualsInPopulation(MutableSet):
+    def __init__(self, deme: Subpopulation):
+        self.individuals = []
+        self.deme = deme.id
+        
+    def __contains__(self, individual: Individual):
+        return individual in self.individuals
+    
+    def __iter__(self):
+        return self.individuals.keys()
+    
+    def __len__(self):
+        return len(self.individuals)
+    
+    def add(self, individual: Individual):
+        self.individuals.append(individual)
+        
+    def discard(self, individual: Individual):
+        del self.individuals[individual]
