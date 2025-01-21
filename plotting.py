5ee17c8184be7a6674a4b896f6c9a09aaa4460b8
mtomasini <matteo.tomasini@lir.gu.se>
2025-01-21 08:25:36 +0100

    A few uncommitted commits

--- plotting.py
+++ plotting.py
@@ -26,3 +26,35 @@ def plot_comparison(dataset_1, dataset_2, title, legend_1, legend_2, output_file
     plt.title(title)
 
     plt.savefig(output_file)
+
+
+
+def metapopulation_plot_comparison(dataset_1, dataset_2, title, legend_1, legend_2, output_file, dataset_3 = None, legend_3 = None):
+    dataset_1_global = pd.read_csv(f"{dataset_1}_metapop_set_counts.csv", index_col=0)
+    dataset_2_global = pd.read_csv(f"{dataset_2}_metapop_set_counts.csv", index_col=0)
+    
+    plt.plot(dataset_1_global.mean(axis=1), color = 'xkcd:blue')
+    plt.plot(dataset_2_global.mean(axis=1), color = 'xkcd:puce')
+    
+    plt.fill_between(dataset_1_global.index, dataset_1_global.mean(axis=1) - dataset_1_global.std(axis=1), dataset_1_global.mean(axis=1) + dataset_1_global.std(axis=1), color='xkcd:blue', alpha=0.3)
+    plt.fill_between(dataset_2_global.index, dataset_2_global.mean(axis=1) - dataset_2_global.std(axis=1), dataset_2_global.mean(axis=1) + dataset_2_global.std(axis=1), color='xkcd:puce', alpha=0.3)
+    
+    plt.axvline(500, color="black", linestyle='--', ymax=1)
+
+    if dataset_3 is not None:
+        dataset_3_global = pd.read_csv(f"{dataset_3}_metapop_set_counts.csv", index_col=0)
+        plt.plot(dataset_3_global.mean(axis=1), color = 'xkcd:light purple')
+        plt.fill_between(dataset_3_global.index, dataset_3_global.mean(axis=1) - dataset_3_global.std(axis=1), dataset_3_global.mean(axis=1) + dataset_3_global.std(axis=1), color='xkcd:light purple', alpha=0.3)
+
+
+    if dataset_3 is not None:
+        plt.legend([f"{legend_1}", f"{legend_2}", f"{legend_3}"])
+    else:
+        plt.legend([f"{legend_1}", f"{legend_2}"])
+
+    plt.ylabel("Number of feature sets")
+    plt.xlabel("Generations (x100)")
+    plt.grid(linestyle=':', linewidth=0.5)
+    plt.title(title)
+
+    plt.savefig(output_file)
